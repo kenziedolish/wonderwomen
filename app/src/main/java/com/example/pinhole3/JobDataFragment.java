dc5f05d455b2af357802c007c497dcac02254545
kenziedolish <mackisback161@gmail.com>
2019-12-08 20:04:41 -0500

    merged with jobdto code

--- app/src/main/java/com/example/pinhole3/JobDataFragment.java
+++ app/src/main/java/com/example/pinhole3/JobDataFragment.java
@@ -77,7 +77,7 @@ public class JobDataFragment extends Fragment implements MyRecyclerViewAdapter.I
 
     @Override
     public void onItemClick(View view, int position) {
-        Toast.makeText(this.getContext(), "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this.getContext(), "You clicked " + adapter.getItem(position), Toast.LENGTH_SHORT).show();
 
         state = adapter.getItem(position);
         Intent intent = new Intent(view.getContext(),ViewJobDatabase.class );
